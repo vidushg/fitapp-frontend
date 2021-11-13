08957b0e5cb9a3112bf9e64b7050c320e87e7eee
Vidush Goswami <goswamiv@grinnell.edu>
2021-11-13 13:26:58 -0600

    setup ui to consume API data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
+//import { response } from 'express';
 
-function App() {
+class App extends Component {
+
+constructor(props) {
+  super(props);
+  this.state = {
+    message: ""
+  }
+}
+
+componentDidMount() {
+  return fetch ('api/hello')
+    .then((response) => response.json())
+    .then ((responseJson) => {
+      this.setState({
+        message: responseJson.message
+      });
+    })
+}
+
+render(){
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="App-intro">
+      Message from API: <b>{this.state.message}</b>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
+}
 }
 
 export default App;
