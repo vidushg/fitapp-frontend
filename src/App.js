3387712eacdae3113a08a254d50da2b9151f15a2
Vidush Goswami <goswamiv@grinnell.edu>
2021-11-16 00:02:26 -0600

    modified api call

--- src/App.js
+++ src/App.js
@@ -8,18 +8,31 @@ class App extends Component {
 constructor(props) {
   super(props);
   this.state = {
-    message: ""
+    data: ""
   }
 }
 
 componentDidMount() {
-  return fetch ('api/hello')
+  this.callBackend()
+    .then(res => this.setState({data: res.message}))
+    .catch(err => console.log(err))
+  /*return fetch ('api/hello')
     .then((response) => response.json())
     .then ((responseJson) => {
       this.setState({
         message: responseJson.message
       });
-    })
+    })*/
+}
+
+callBackend = async() => {
+  const response = await fetch('api/hello');
+  const body = response.json();
+
+  if(response.status !== 200){
+    throw Error(body.error)
+  }
+  return body;
 }
 
 render(){
@@ -28,7 +41,7 @@ render(){
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p className="App-intro">
-      Message from API: <b>{this.state.message}</b>
+      Message from API: <b>{this.state.data}</b>
         </p>
         
       </header>
